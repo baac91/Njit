d6b705ba8c41772b1bf19d8d28550a740ef44d8a
brian chin rutg student <bc73@afsconnect1.njit.edu>
2013-02-11 22:43:05 -0500

    array

---
+++ array.js
@@ -0,0 +1,11 @@
+var myArray = [500,500,500,500,500];
+
+var total = 0;
+
+for ( var i = 0 ; i < myArray.length ; i++ ) {
+  	
+    total = total + myArray[i];
+}
+
+
+alert("The total is: " + total);
